a22387f66475821c8eccff5991fe375355f00ba6
Leticia Rossi <lrossidossantos18@amherst.edu>
2017-03-09 12:34:52 +0100

    Changed player's collider so won't get randomly stuck on the floor again

---
+++ 2D-Platformer/Assets/Scripts/PresserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresserController : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		Vector3 pos = transform.position; 
+		pos.y -= 0.3f; 
+		transform.position = pos; 
+	}
+
+}
