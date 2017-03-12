995c67242bd808687f8f41bb00b2eb1dc4ea2294
Leticia Rossi <lrossidossantos18@amherst.edu>
2017-03-12 21:23:25 +0100

    last last changes

---
+++ 2D-Platformer/Assets/Scripts/QuitButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour {
+
+	public Button quitButton; 
+
+	void Start () {
+		Button btn = quitButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Application.Quit ();
+	}
+}
